0e70a6b7ef430b70d515a5201833409b163c072f
CommandermineYT <andre.braun.andre@gmail.com>
2026-08-18 21:00:48 +0200

    Delete src/src directory

--- src/src/main/java/com/totalecollapse/mixin/ExampleMixin.java
+++
@@ -1,9 +0,0 @@
-package com.totalecollapse.mixin;
-
-// This mixin needs to be moved to src/client/java/ since it targets Minecraft class (client-side only)
-// For now, we'll remove it from the mixin config and keep just an empty file
-
-public class ExampleMixin {
-    // Empty - will be removed or replaced with server-side mixin if needed
-
-}
