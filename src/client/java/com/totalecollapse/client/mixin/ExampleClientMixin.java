a7b6c7c55752316fe9e220d9a5f2e2f7b8142b66
CommandermineYT <andre.braun.andre@gmail.com>
2026-08-18 17:26:34 +0200

    Add files via upload

---
+++ src/client/java/com/totalecollapse/client/mixin/ExampleClientMixin.java
@@ -0,0 +1,22 @@
+package com.totalecollapse.client.mixin;
+
+// @import net.minecraft.client.Minecraft;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// Mixin commented out until you have a specific target method to inject into
+
+public class ExampleClientMixin {
+    // @Mixin(Minecraft.class)
+    // public abstract class ExampleClientMixin {
+        // Placeholder - remove or add actual mixin code when needed
+    
+        // @Inject(at = @At("HEAD"), method = "YOUR_TARGET_METHOD_NAME", cancellable = true)
+        // private void yourMethod(CallbackInfo info) {
+        //     // Your injection code here
+        // }
+    // }
+}
+
